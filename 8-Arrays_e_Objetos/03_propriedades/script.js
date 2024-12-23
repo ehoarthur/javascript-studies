acf838d731f4fee80a8392564f8b2b817dad9d2b
Arthur Danielson <arthurdanielson7@gmail.com>
2024-12-23 19:55:13 -0400

    23-12-2024

---
+++ 8-Arrays_e_Objetos/03_propriedades/script.js
@@ -0,0 +1,7 @@
+let nome = "João";
+console.log(nome.length);
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numeros.length);
+console.log(numeros["length"]);
+console.log(numeros[1]);
