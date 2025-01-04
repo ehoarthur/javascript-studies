863ad6318196dbb1df7195a8f5ce08a651874459
Arthur Danielson <arthurdanielson7@gmail.com>
2025-01-04 15:41:02 -0400

    03-01-2025 #2

---
+++ 15-Programacao_Assincrona/03_promise_falha/script.js
@@ -0,0 +1,7 @@
+let p = Promise.resolve(new Error("Não deu certo"));
+
+console.log("lalala");
+
+p.then((value) => console.log(value)).catch((reason) =>
+  console.log("Falhou: " + reason)
+); // Não deu certo
